f72720a1fef7d238a81638e93d7ae48447ed4004
nemel1dasx <maxim130909@gmail.com>
2023-07-27 18:26:31 +0600

    feat: do js-hw

--- scripts/app.js
+++ scripts/app.js
@@ -1,65 +1,49 @@
-let firstName = prompt("Введите ваше имя:");
-let lastName = prompt("Введите вашу фамилию:");
-let nickname = prompt("Введите ваш никнэйм:");
-
-let hello = "Привет, ";
-
-if (firstName || lastName || nickname) {
-  hello += firstName || "";
-  hello += lastName ? " " + lastName : "";
-  hello += nickname ? " (" + nickname + ")" : "";
-} else {
-  hello += "noname";
-}
-
-alert(hello);
-
-
-
-let userInput = prompt("Введите число:");
-
-if (isNaN(userInput)) {
-  alert("Вы ввели не число!");
-} else {
-  let number = parseFloat(userInput);
-  let message = "";
-
-  message += number % 2 === 0 ? "Четное" : "Нечетное";
-  message += Number.isInteger(number) ? " и целое" : " и дробное";
-  message += number > 100 ? ", больше 100" : "";
-  message += number < 100 ? ", меньше 100" : "";
-
-  alert(message);
-}
-
-
-let num1 = parseFloat(prompt("Введите первое число:"));
-let num2 = parseFloat(prompt("Введите второе число:"));
-
-let sum = num1 + num2;
-alert("Сумма чисел: " + sum);
-
-
-let age = parseInt(prompt("Введите ваш возраст:"));
-let acsess = age > 17 ? "Доступ разрешен" : "Доступ закрыт";
-alert(access);
-
-
-let userNumber = prompt("Введите число или строку:");
-let number = parseFloat(userInput);
-
-if (isNaN(number)) {
-  alert("Вы ввели не число: " + userNumber);
-} else {
-  switch (true) {
-    case number > 10:
-      alert("Число больше 10");
-      break;
-    case number <= 10:
-      alert("Число меньше или равно 10");
-      break;
+/* function stringCutter(text) {
+  const maxLength = 30;
+  if (text.length <= maxLength) {
+    return text;
+  } else {
+    const shortenedText = text.substring(0, maxLength - 3) + "...";
+    return shortenedText;
   }
 }
 
+let firstSentence = "But you have no right to call me a murderer";
+let secondSentence = "Because they could stand that these were not monsters.";
+let thirdSentence = "And I want to remember it. I never want to forget it.";
 
+console.log(stringCutter(firstSentence));
+console.log(stringCutter(secondSentence));
+console.log(stringCutter(thirdSentence)); */
 
+/* function registerInversion(text) {
+  let invertedText = '';
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    if (char === char.toUpperCase()) {
+      invertedText += char.toLowerCase();
+    } else {
+      invertedText += char.toUpperCase(); 
+    }
+  }
+  return invertedText;
+}
+
+console.log(registerInversion('WTF'));
+console.log(registerInversion('Be my Valentine'));
+console.log(registerInversion('Hello  World!')); */
+
+function bannedLetters(sentence, bannedWord) {
+  const regex = new RegExp(`\\b${bannedWord}\\b`, "gi");
+  const bannedSentence = sentence.replace(regex, "******");
+  return bannedSentence;
+}
+
+let firstSentence = "Follow the white raBbiT";
+let secondSentence = "Call me Ishmael";
+let thirdSentence = "Repent before bloody rabbit punish you!";
+let bannedWord = "rabbit";
+
+console.log(bannedLetters(firstSentence, bannedWord));
+console.log(bannedLetters(secondSentence, bannedWord));
+console.log(bannedLetters(thirdSentence, bannedWord));
